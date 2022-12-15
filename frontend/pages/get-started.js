ea6c5ad734b6416a86a1b74c4d031e956457fcc7
nishanprime <thapanishan9@gmail.com>
2022-12-14 22:27:29 -0500

    new backend changes

--- frontend/pages/get-started.js
+++ frontend/pages/get-started.js
@@ -122,6 +122,7 @@ const GetStarted = () => {
                   {/* <div className="text-sm">
                     <a
                       href="#"
+                     
                       className="font-medium text-indigo-600 hover:text-indigo-500"
                     >
                       Forgot your password?
