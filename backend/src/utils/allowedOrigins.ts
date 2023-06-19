ad02ade6c817a130caece6650b04dedb59a8b90f
nishanprime <nt554@drexel.edu>
2023-06-19 13:51:18 -0400

    fixed cors error v2

--- backend/src/utils/allowedOrigins.ts
+++ backend/src/utils/allowedOrigins.ts
@@ -1,16 +1,10 @@
 import cors from "cors";
 
 const allowedOrigins = (app) => {
-  let allowedOrigins;
-  console.log(process.env.NODE_ENV);
-  if (process.env.NODE_ENV === "production") {
-    allowedOrigins = "https://dynamicportfolio.io";
-  } else if (process.env.NODE_ENV === "development") {
-    allowedOrigins = "http://localhost:3000";
-  }
   app.use(
     cors({
-      origin: allowedOrigins,
+      origin: true,
+      credentials: true,
     })
   );
 };
